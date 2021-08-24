1acbccf714d87d233a111e0ea5c86bba9df62db2
dr-rosen-rosen <mrosen44@jhmi.edu>
2021-08-24 08:17:17 -0400

    Minor format changes

--- 0_main.R
+++ 0_main.R
@@ -12,14 +12,14 @@ library(reticulate)
 library(here)
 library(readxl)
 
-debuggingState(on=FALSE)
+debuggingState(on = FALSE)
 # start ve with: source python3/bin/activate in project folder
 Sys.setenv(R_CONFIG_ACTIVE = "mike") # 'default')#
 config <- config::get()
 Sys.setenv(RETICULATE_PYTHON = config$py_version)
-reticulate::source_python('1_funcs.py')
-source(here('1_funcs.R'), echo = TRUE)
-#source(here('2a_connect.R'), echo = TRUE)
+reticulate::source_python("1_funcs.py")
+source(here("1_funcs.R"), echo = TRUE)
+# source(here('2a_connect.R'), echo = TRUE)
 
 
 tasks_df <- get_task_list(
@@ -29,24 +29,25 @@ tasks_df <- get_task_list(
 
 
 sync_df <- get_E4_synchronies(
-  tasks_df = tasks_df[1:100, ],
+  tasks_df = tasks_df[1:100,],
   offset = config$sync_offset,
   roles = config$sync_roles,
   measures = config$sync_measures,
-  sampling_freq = config$sync_sampling_freq, 
-  corr_method = config$sync_corr_method, 
-  use_residuals = config$synch_use_residuals, 
-  missing_E4_flag = config$sync_missing_E4_flag, 
-  NA_E4_flag = config$sync_NA_E4_flag, 
-  db_path = paste0(config$root_data_dir,'/',config$sync_db_path),
+  sampling_freq = config$sync_sampling_freq,
+  corr_method = config$sync_corr_method,
+  use_residuals = config$synch_use_residuals,
+  missing_E4_flag = config$sync_missing_E4_flag,
+  NA_E4_flag = config$sync_NA_E4_flag,
+  db_path = paste0(config$root_data_dir, "/", config$sync_db_path),
   save_csv = FALSE
 )
 
 sync_df_long <- reshape_sync_data(
   sync_df = sync_df,
-  tasks_df = tasks_df[1:100, ],
+  tasks_df = tasks_df[1:100,],
   roles = config$sync_roles,
   measures = config$sync_measures,
   metrics = config$sync_metrics,
   data_dir = config$root_data_dir,
-  save_csv = FALSE)
+  save_csv = FALSE
+)
